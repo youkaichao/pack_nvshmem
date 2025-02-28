31bbcef3dce51da7ffbbfe66edb905ba353a962e
youkaichao <youkaichao@gmail.com>
2025-02-28 21:18:02 +0800

    add readme
    Signed-off-by: youkaichao <youkaichao@gmail.com>

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="vllm_nvshmem",
-    version="0.1",
-    packages=["vllm_nvshmem"],
-    package_data={"vllm_nvshmem": ["**/*.*"]},
-    include_package_data=True,
-)
